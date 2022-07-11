505ede15dc262332e9ea0ac5c8ece42c5d2b1a49
Thuan <thuanvo326@gmail.com>
2022-07-11 18:02:13 +0700

    3rd commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/userController')
 
-router.post('/', (req, res) => {
-    res.send('Register route')
-})
+router.post('/', registerUser)
+
+router.post('/login', loginUser)
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+const { urlencoded } = require('body-parser')
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the support desk API'})
@@ -11,5 +16,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
