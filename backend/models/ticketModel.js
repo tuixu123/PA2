e242c37b30bb817c1038d8ee3ca3dbeaf1e6fc9d
Thuan <thuanvo326@gmail.com>
2022-07-16 13:27:44 +0700

    working on tickets

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,31 @@
+const mongooese = require("mongoose");
+
+const ticketSchema = mongooese.Schema(
+  {
+    user: {
+      type: mongooese.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    product: {
+      type: String,
+      required: [true, "Please select a product"],
+      enum: ["iPhone", "Macbook Pro", "iMac", "iPad"],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter a description of the issue"],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["new", "open", "closed"],
+      default: "new",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongooese.model("Ticket", ticketSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
-const { urlencoded } = require('body-parser')
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
-const PORT = process.env.PORT || 5000
+const { urlencoded } = require("body-parser");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
 
 // connect to DB
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the support desk API'})
-})
-
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the support desk API" });
+});
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
